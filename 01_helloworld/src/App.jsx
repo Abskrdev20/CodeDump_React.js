46ca6de628f378cb5933534b2006e70370aeeaa5
Abhishek Kumar <absk.devopsmastero@gmail.com>
2025-07-18 20:48:13 +0530

    Created Custom Componets

--- 01_helloworld/src/App.jsx
+++ 01_helloworld/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import Coffee from './Coffee'
 
 function App() {
-
-
   return (
+    <>
+    {/* returning a fragment */}
     <h1>React with Vite | Abhishek Kumar</h1>
+    <Coffee/>
+    </>
   )
 }
 
---
+++ 01_helloworld/src/Coffee.jsx
@@ -0,0 +1,9 @@
+
+// creating a components in react
+function Coffee() {
+    return (
+        <h2>Coffee with React</h2>
+    )
+}
+
+export default Coffee
